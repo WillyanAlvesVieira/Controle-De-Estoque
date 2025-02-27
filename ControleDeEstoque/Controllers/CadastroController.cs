ced8e70dd2a761facf19b5dc3af5aab9f3fdffb8
Willyan Alves <Willyan.a.vieira@outlook.com>
2025-02-27 19:46:27 -0300

    Add project files.

---
+++ ControleDeEstoque/Controllers/CadastroController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeEstoque.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult GrupoProduto() {  return View(); }
+        public IActionResult MarcaProdutos() { return View(); }
+        public IActionResult LocaisArmazenamento() { return View(); }
+        public IActionResult UnidadesMedida() { return View(); }
+        public IActionResult Produto() { return View(); }
+        public IActionResult Pais() { return View(); }
+        public IActionResult Estado() { return View(); }
+        public IActionResult Cidade() { return View(); }
+        public IActionResult Fornecedores() { return View(); }
+        public IActionResult PerfilUsuarios() { return View(); }
+        public IActionResult Usuario() { return View(); }
+    }
+}
---
+++ ControleDeEstoque/Controllers/OperacoesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeEstoque.Controllers
+{
+    public class OperacoesController : Controller
+    {
+        public IActionResult EntradaEstoque() { return View(); }
+        public IActionResult SaidaEstoque() { return View(); }
+        public IActionResult LancPerdaProduto() { return View(); }
+        public IActionResult Inventario() { return View(); }
+    }
+}
